04f60d3ef42ac6abaddc515a0d85dcb16a71007d
ruifanxu <ruifanxu@usc.edu>
2024-10-20 23:47:30 -0700

    GUI update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,39 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { ResizeListenerService } from '../../services/resize-lisenter.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   width: number = 0;
   height: number = 0;
-  isMenuOpen=false;
-  smallScreen=false;
-  constructor(private resizeService: ResizeListenerService) {}
+  isMenuOpen = false;
+  smallScreen = false;
+  @ViewChild('menuIcon') menuIconRef!: ElementRef;
+
+  constructor(
+    private resizeService: ResizeListenerService,
+    private elementRef: ElementRef
+  ) {}
 
   ngOnInit(): void {
-    this.resizeService.resize$.subscribe(size => {
+    this.resizeService.resize$.subscribe((size) => {
       this.width = size.width;
       this.height = size.height;
-      console.log(`Parent Component - Width: ${this.width}, Height: ${this.height}`);
+      console.log(
+        `Parent Component - Width: ${this.width}, Height: ${this.height}`
+      );
     });
-    this.resizeService.smallScreen$.subscribe(smallScreen => {
+    this.resizeService.smallScreen$.subscribe((smallScreen) => {
       this.smallScreen = smallScreen;
     });
   }
 
-  get menuMode(){
-    if (this.width <= 576){
-      return "inline"
-    }else {
-      return "horizontal"
+  get menuMode() {
+    if (this.width <= 576) {
+      return 'inline';
+    } else {
+      return 'horizontal';
     }
   }
 
   // Toggle the menu open/close
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  onNavigate(){
+    this.isMenuOpen = false;
+  }
+
+  // Detect click outside the menu to close it
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+    if (!clickedInside) {
+      this.isMenuOpen = false;
+    }
   }
 }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  form!: FormGroup;
+  emailUrl:string="https://formsubmit.co/ruifan831@gmail.com";
+  constructor(private fb:FormBuilder,private http: HttpClient){}
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name:[],
+      email:[],
+      message:[]
+    })
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      const formData = this.form.value;
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+      this.http.post(this.emailUrl, formData, { headers })
+        .subscribe({
+          next: (response) => {
+            console.log('Success:', response);
+          },
+          error: (error) => {
+            console.error('Error:', error);
+          }
+        });
+    }
+  }
 
 }
